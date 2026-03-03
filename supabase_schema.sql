43e9512f9f39badc5136461a29a0c1b15a3ca5c3
Christophe Saugé <csauge@gmail.com>
2026-03-03 10:57:38 +0100

    security: fix mutable search_path in PostgreSQL function

--- supabase_schema.sql
+++ supabase_schema.sql
@@ -2,12 +2,15 @@
 
 -- 0. Helper function for updated_at
 CREATE OR REPLACE FUNCTION update_updated_at_column()
-RETURNS TRIGGER AS $$
+RETURNS TRIGGER 
+LANGUAGE plpgsql
+SET search_path = ''
+AS $$
 BEGIN
     NEW.updated_at = now();
     RETURN NEW;
 END;
-$$ language 'plpgsql';
+$$;
 
 -- 1. Resources Table (Public Catalog)
 CREATE TABLE resources (
